3b7a46d226da844e02e256235722a8187fb64b19
Sean Hastings <sean_hastings@alumni.brown.edu>
2023-06-09 17:22:49 -0400

    multiprocessing re-added, but batch sizes > 1 not working atm

--- winnow/feature_extraction/extraction_routine.py
+++ winnow/feature_extraction/extraction_routine.py
@@ -23,7 +23,7 @@ def process_videos(tasks: Collection) -> Collection:
     return [process_video(task) for task in tasks]
 
 
-def process_video(task: Tuple[str, Any, int, int]) -> Tuple[Any, np.ndarray]:
+def process_video(task: Tuple[str, Any, int]) -> Tuple[Any, np.ndarray]:
     """Process a single video.
 
     Takes a tuple (video_path, video_id, model, frame_sampling, batch_size).
@@ -34,7 +34,7 @@ def process_video(task: Tuple[str, Any, int, int]) -> Tuple[Any, np.ndarray]:
     logger.debug("Preparing frames for %s", video_path)
     frames_tensor = load_video(video_path, frame_sampling)
     logger.debug("Done preparing frames for %s", video_path)
-    return video_id, frames_tensor
+    return (video_id, frames_tensor)
 
 
 # Type hint for a function tha will be called when a particular
@@ -81,30 +81,48 @@ def feature_extraction_videos(
         )
     )
     batches = [tasks[i * batch_sz : (i + 1) * batch_sz] for i in range(len(tasks) // batch_sz + 1)]
+    batches = [batch for batch in batches if len(batch) > 0]
+    logger.debug(f"{len(batches)} batches")
+    for batch in batches:
+        logger.debug(len(batch))
 
-    with torch.no_grad():
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        model.to(device)
+    # Chunk tasks into as close as possible to 1,0000 chunks, but with chunksize bounded [1, 10] (inclusive)
+    chunksize = max(min(len(tasks) // 10000, 10), 1)
 
-        progress_bar = iter(tqdm(range(file_count), mininterval=1.0, unit="image"))
-        semaphore = mp.Semaphore(cores)
-        try:
-            # with mp.Pool(cores) as pool:
-            # for batch in pool.imap_unordered(process_videos, semaphore_producer(semaphore, batches)):
-            for batch in batches:
-                ids, images = zip(*process_videos(batch))
-                batch_data = torch.stack(images, dim=0)
-                logger.debug(f"Extracting features for {len(ids)} files.")
-                batch_features = model.extract(batch_data.to(device)).cpu().numpy()
-                logger.debug("Extracted, processing")
-                for image_id, image_features in zip(ids, [batch_features[i] for i in range(batch_features.shape[0])]):
-                    on_extracted(image_id, image_features)
-                    next(progress_bar)
-                logger.debug("Processed.")
-                semaphore.release()
-        except Exception as e:
-            logger.error(f"{e}")
-            raise e
+    # Semaphore pattern used below from https://stackoverflow.com/questions/30448267/multiprocessing-pool-imap-unordered-with-fixed-queue-size-or-buffer
+    semaphore = mp.Semaphore(cores * chunksize)
+
+    progress_bar = iter(tqdm(range(file_count), mininterval=1.0, unit=" image"))
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    try:    
+        with torch.no_grad():
+            with mp.Pool(cores) as pool:
+                for batch_outputs in pool.imap_unordered(
+                    process_videos, semaphore_producer(semaphore, batches), chunksize=chunksize
+                ):
+                    logger.debug(batch_outputs)
+                    if len(batch_outputs) == 0:
+                        logger.warn("Unknown glyph failed to process, skipping.")
+                        semaphore.release()
+                        continue
+                    ids, images = list(zip(*batch_outputs))
+                    batch_data = torch.stack(images, dim=0)
+                    logger.debug(f"Extracting features for {len(ids)} files.")
+                    batch_features = model.extract(batch_data.to(device)).cpu().numpy()
+                    logger.debug(batch_data.shape)
+                    logger.debug(batch_features.shape)
+                    logger.debug("Extracted, processing")
+                    for image_id, image_features in zip(ids, [batch_features[i] for i in range(batch_features.shape[0])]):
+                        logger.debug(image_id)
+                        on_extracted(image_id, image_features)
+                        next(progress_bar)
+                    logger.debug("Processed.")
+                    semaphore.release()
+                    logger.debug("Released.")
+    except Exception as e:
+        logger.error(f"{e}")
+        raise e
 
 
 def semaphore_producer(semaphore: mp.Semaphore, tasks: Collection[tuple]):
