26fcea72eef568a5fd3224abdcda12a9fdc336a1
Sean Hastings <sean_hastings@alumni.brown.edu>
2023-06-09 21:03:52 -0400

    winnow image loading

--- winnow/feature_extraction/utils.py
+++ winnow/feature_extraction/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from torchvision import transforms
-from torchvision.io import read_image
+import PIL.Image as Image
 
 
 class FloorCeil(object):
@@ -16,11 +16,17 @@ class FloorCeil(object):
 
 TRANSFORMS = [
     transforms.Resize((224, 224)),
+    transforms.Grayscale(),
+    transforms.ToTensor(),
     transforms.ConvertImageDtype(torch.float32),
-    # FloorCeil(),
+    #FloorCeil(),
     transforms.Normalize((0.5,), (0.5,)),
 ]
 
+def read_image(path, shape=(224, 224)):
+    im = Image.open(path)  
+    im.draft('L',shape)
+    return im
 
 def load_video(video, frame_sampling) -> np.ndarray:
     """
@@ -37,13 +43,8 @@ def load_video(video, frame_sampling) -> np.ndarray:
             Exception: if provided video can not be load
     """
     try:
-        image = read_image(video).cpu()
-        if image.shape[-3] == 3:
-            transform = transforms.Compose([transforms.Grayscale()] + TRANSFORMS)
-        elif image.shape[-3] == 1:
-            transform = transforms.Compose(TRANSFORMS)
-        else:
-            raise Exception(f"Image loaded with invalid shape {image.shape}.")
+        image = read_image(video)
+        transform = transforms.Compose(TRANSFORMS)
         image = transform(image)
         return image
     except Exception as e:
